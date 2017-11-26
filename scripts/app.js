c00e810acdec1e2024eeacee3126b1b095125af7
Marius SODJINOU <desmart92@gmail.com>
2017-11-26 15:01:21 +0100

    Initial commit

--- scripts/app.js
+++ scripts/app.js
@@ -1,26 +1,17 @@
 (function() {
         'use strict';
-    angular.module('pag-site', ["ui.router","ap.controllers", "ap.services","ngMaterial"])
-      .config(function ($stateProvider, $urlRouterProvider, $httpProvider,$authProvider) {
-    
-        // Satellizer configuration that specifies which API
-        // route the JWT should be retrieved from
-        $authProvider.loginUrl = 'http://localhost/geopal/servicesbackend/public/api/v1/auth';
-    
-          // For any unmatched url, redirect to /login
+    angular.module('pag-site', ["ui.router","ap.controllers", "ap.services"])
+      .config(function ($stateProvider, $urlRouterProvider, $httpProvider) {
          $urlRouterProvider.otherwise("/home");
-    
           // Now set up the states
           $stateProvider
           // HOME
           .state('home', {
               url: "/",
               controller: "HomeUserCtrl",
-              templateUrl: "views/user/login.html"
+              templateUrl: "views/site/home.html"
           })
-    ; // End stateProvider
-    
+            ; // End stateProvider
         });
-    
     })();
     
