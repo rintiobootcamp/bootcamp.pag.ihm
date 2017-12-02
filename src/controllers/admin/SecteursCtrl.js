684eaf02494beb6bf13267e6efba9e92b06d3901
Mashkour <azizmashkour@gmail.com>
2017-12-02 20:17:59 +0100

    Query piliers from new secteur form

--- src/controllers/admin/SecteursCtrl.js
+++ src/controllers/admin/SecteursCtrl.js
@@ -1,12 +1,27 @@
 angular.module('pag-site')
-    .controller("AdminSecteursCtrl", function (ModelSecteur, $scope) {
-        var getListSecteurs = function () {
-            ModelSecteur.list()
-                .then( function(data) {
-                    $scope.listSecteurs = data.data;
-                }, function (error) {
-                    console.log(error);
-                });
-        }
-        getListSecteurs();
+.controller("AdminSecteursCtrl", function (ModelSecteur, $scope) {
+  var getListSecteurs = function () {
+    ModelSecteur.list()
+    .then( function(data) {
+      $scope.listSecteurs = data.data;
+    }, function (error) {
+      console.log(error);
     });
+  }
+  getListSecteurs();
+})
+.controller("AdminNewSecteurCtrl", function (ModelPilier, $scope) {
+  console.log('Admin Nouveau secteur controller');
+
+  var getListPiliers = function () {
+      ModelPilier.list()
+          .then( function(data) {
+              $scope.listPiliers = data.data;
+          }, function (error) {
+              console.log(error);
+          });
+  }
+  getListPiliers();
+
+  $scope.pilier = {name: ''};
+});
