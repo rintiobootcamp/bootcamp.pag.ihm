d39c861c337d62ec37f588fd3741890fd1baaca5
Mashkour <azizmashkour@gmail.com>
2017-12-02 18:06:11 +0100

    Create Admin project controller

---
+++ src/controllers/admin/ProjetsCtrl.js
@@ -0,0 +1,12 @@
+angular.module('pag-site')
+.controller("AdminProjetsCtrl", function (ModelProjet, $scope) {
+    var getListProjets = function () {
+        ModelProjet.list()
+            .then( function(data) {
+                $scope.listProjets = data.data;
+            }, function (error) {
+                console.log(error);
+            });
+    }
+    getListProjets();
+})
