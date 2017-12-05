530bc253382ff3e6641c867a4edac018f967459c
Marius SODJINOU <desmart92@gmail.com>
2017-12-05 19:38:58 +0100

    Change media method

--- src/services/MediaFactory.js
+++ src/services/MediaFactory.js
@@ -5,13 +5,15 @@ angular.module('pag-site')
         return $http({
           method: 'GET',
           url: API.url + '/media/get_media.json'
+          //url: API.media_fonct_url + '?entityId=' + params.entityId + '&entityType=' + params.entityType
         });
       },
       save: function (obj){
         return $http({
           method: 'POST',
           data: angular.toJson(obj),
-          url: API.url + '/media/post_media.json'
+          //url: API.url + '/media/post_media.json'
+          url: API.media_fonct_url
         })
       }
     }
