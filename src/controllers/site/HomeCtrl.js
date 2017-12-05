380e29d9ed874fea1bbe789d79b3800dbc93aa30
Hadjara Idriss <idris.hadjara@yahoo.com>
2017-12-05 15:56:20 +0100

    Added markers to map

--- src/controllers/site/HomeCtrl.js
+++ src/controllers/site/HomeCtrl.js
@@ -1,5 +1,5 @@
 angular.module('pag-site')
-.controller("SiteHomeCtrl", function (ModelProjet, ModelAxe, $scope) {
+.controller("SiteHomeCtrl", function (ModelProjet, ModelAxe, $scope, leafletMapEvents) {
   $scope.mapDefaults = {
     scrollWheelZoom: false
   };
@@ -17,57 +17,6 @@ angular.module('pag-site')
 
   var initializeMap = function (){
     $scope.listProjetsRegions = [];
-    var markers = [
-      {
-        lat: 7.876355,
-        lng: 2.284468,
-        message: "My Added Marker",
-        icon: {
-          iconUrl: '/node_modules/leaflet/dist/images/marker-icon.png',
-          shadowUrl: '/node_modules/leaflet/dist/images/marker-shadow.png',
-        }
-      },
-
-      {
-        lat: 6.522945,
-        lng: 2.666289,
-        message: "Ouémé",
-        icon: {
-          iconUrl: '/node_modules/leaflet/dist/images/marker-icon.png',
-          shadowUrl: '/node_modules/leaflet/dist/images/marker-shadow.png',
-        }
-      },
-
-      {
-        lat: 9.389607,
-        lng:  2.634361,
-        message: "Parakou",
-        icon: {
-          iconUrl: '/node_modules/leaflet/dist/images/marker-icon.png',
-          shadowUrl: '/node_modules/leaflet/dist/images/marker-shadow.png',
-        }
-      },
-
-      {
-        lat: 10.189803,
-        lng: 1.452991,
-        message: "Nati",
-        icon: {
-          iconUrl: '/node_modules/leaflet/dist/images/marker-icon.png',
-          shadowUrl: '/node_modules/leaflet/dist/images/marker-shadow.png',
-        }
-      },
-
-      {
-        lat: 10.029513,
-        lng:  3.187786,
-        message: "Nikki",
-        icon: {
-          iconUrl: '/node_modules/leaflet/dist/images/marker-icon.png',
-          shadowUrl: '/node_modules/leaflet/dist/images/marker-shadow.png',
-        }
-      }
-    ];
 
     $scope.map = {
       center: {
@@ -94,6 +43,67 @@ angular.module('pag-site')
       },
 
     };
+    $scope.markers = new Array();
+
+    $scope.$on('leafletDirectiveMap.zoomend', function (event, args) {
+      if (args.leafletObject._zoom >= 9) {
+        $scope.map.paths = '';
+
+        var markers = [
+          {
+            lat: 9.357793,
+            lng: 2.601571,
+            message: "Parakou",
+            icon: {
+              iconUrl: '/node_modules/leaflet/dist/images/marker-icon.png',
+              shadowUrl: '/node_modules/leaflet/dist/images/marker-shadow.png',
+            }
+          },
+
+          {
+            lat: 6.522945,
+            lng: 2.666289,
+            message: "Ouémé",
+            icon: {
+              iconUrl: '/node_modules/leaflet/dist/images/marker-icon.png',
+              shadowUrl: '/node_modules/leaflet/dist/images/marker-shadow.png',
+            }
+          },
+
+          {
+            lat: 9.389607,
+            lng:  2.634361,
+            message: "Parakou",
+            icon: {
+              iconUrl: '/node_modules/leaflet/dist/images/marker-icon.png',
+              shadowUrl: '/node_modules/leaflet/dist/images/marker-shadow.png',
+            }
+          },
+
+          {
+            lat: 10.189803,
+            lng: 1.452991,
+            message: "Nati",
+            icon: {
+              iconUrl: '/node_modules/leaflet/dist/images/marker-icon.png',
+              shadowUrl: '/node_modules/leaflet/dist/images/marker-shadow.png',
+            }
+          },
+
+          {
+            lat: 10.029513,
+            lng:  3.187786,
+            message: "Nikki",
+            icon: {
+              iconUrl: '/node_modules/leaflet/dist/images/marker-icon.png',
+              shadowUrl: '/node_modules/leaflet/dist/images/marker-shadow.png',
+            }
+          }
+        ];
+
+        $scope.map.markers = markers;
+      };
+    });
 
   }
 
