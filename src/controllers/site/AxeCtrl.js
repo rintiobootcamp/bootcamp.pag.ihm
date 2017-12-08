932db24d0056e9ae9c21d4dbaafa86dffd564492
Marius SODJINOU <desmart92@gmail.com>
2017-12-08 23:22:19 +0100

    Add comment logic and use $q.all

--- src/controllers/site/AxeCtrl.js
+++ src/controllers/site/AxeCtrl.js
@@ -11,41 +11,17 @@ angular.module('pag-site')
         getListAxes();
         
     })
-    .controller("SiteOneAxeCtrl", function(ModelAxe, ModelMedia, ModelComment, $scope,$stateParams){
-        var getListAxes = function () {
-            ModelAxe.list()
-                .then( function(data) {
-                    $scope.listAxes = data.data;
-                }, function (error) {
-                    console.log(error);
-                });
-        }
-        getListAxes();
+    .controller("SiteOneAxeCtrl", function(ModelAxe, ModelMedia, ModelComment, $scope,$stateParams,$q){
 
         var params_get_entity = {
             id:$stateParams.id
         }
 
-        var getAxe = function (params){
-            ModelAxe.get(params)
-                .then( function(data) {
-                    console.log(data);
-                    $scope.axe = data.data;
-                }, function (error) {
-                    console.log(error);
-                });
-        }
-        getAxe(params_get_entity);
-
-        $scope.getImage = function (axe){
-            ModelMedia.list(axe.id)
-                .then( function(data) {
-                    axe.img = data.data[0].link;
-                }, function (error) {
-                    console.log(error);
-                });
-                return CONST.defaultImageEntity;
-        }
+        $q.all([ModelAxe.list(), ModelAxe.get(params_get_entity)])
+        .then( values => {
+            $scope.listAxes = values[0].data;
+            $scope.axe = values[1].data;
+        });
 
         var params_get_comments = {
             entityId:$stateParams.id,
@@ -61,5 +37,60 @@ angular.module('pag-site')
                 });
         }
         getListComments(params_get_comments);
+
+        $scope.comment = {
+            show:false,
+            id:0,
+            pseudo:'',
+            email:'',
+            contenu:''
+        }
+    
+        $scope.showComment = function (){
+            $scope.comment.show = true;
+        }
+    
+        $scope.postComment = function (){
+            var params = {
+                entityType : "AXE",
+                entityId : parseInt($stateParams.id),
+                pseudo: $scope.comment.pseudo,
+                userMail : $scope.comment.email,
+                contenu : $scope.comment.contenu
+            }
+            ModelComment.post(params)
+                .then(function(data){
+                    getListComments(params_get_comments);
+                    $scope.comment.show = false;
+    
+                    $scope.comment.id = data.data.id;
+                    uploadFiles($scope.comment.files);
+    
+                }, function (error){
+                    console.log(error);
+                });
+        }
+    
+        // upload on file select or drop
+        $scope.uploadFile = function (file) {
+            Upload.upload({
+                url: API.media_fonct_url + 'COMMENTAIRE/' + $scope.comment.id,
+                data: {file: file}
+            }).then(function (resp) {
+                console.log(resp);
+            }, function (resp) {
+                console.log(resp.status);
+            }, function (evt) {
+    
+            });
+        };
+        // for multiple files:
+        $scope.uploadFiles = function (files) {
+          if (files && files.length) {
+            for (var i = 0; i < files.length; i++) {
+                uploadFile(files[i]);
+            }
+          }
+        }
     })
 ;
