a9c0cad8cf5e4e80998f4cc3dd90d57ff60728b2
Marius SODJINOU <desmart92@gmail.com>
2017-12-09 16:35:17 +0100

    Added file to project

---
+++ src/services/LikeFactory.js
@@ -0,0 +1,17 @@
+angular.module('pag-site')
+    .factory('ModelLike', function ($http, API) {
+        return {
+            get: function (params){
+                return $http({
+                    method: 'GET',url: API.like_fonct_url + '/' + params.entityType +'/' + params.entityId
+                });
+            },
+            post: function (obj){
+                return $http({
+                    method: 'POST',
+                    data: angular.toJson(obj),
+                    url: API.like_fonct_url
+                })
+            }
+        }
+    });
