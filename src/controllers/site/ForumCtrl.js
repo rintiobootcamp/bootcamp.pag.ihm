dc8275e794ff0ca90afd8050203c5a1d3e9cd424
Marius SODJINOU <desmart92@gmail.com>
2017-12-13 00:41:11 +0100

    Create model forum

--- src/controllers/site/ForumCtrl.js
+++ src/controllers/site/ForumCtrl.js
@@ -1,4 +1,12 @@
 angular.module('pag-site')
-  .controller("SiteForumCtrl", function ($scope, $state) {
-    
+  .controller("SiteForumCtrl", function ($scope,$q, ModelSecteur, ModelPilier, ModelAxe, ModelProjet) {
+    $q.all([ModelSecteur.list(),ModelPilier.list(),ModelAxe.list(), ModelProjet.list()])
+      .then(values => {
+        $scope.listSecteurs = values[0].data;
+        $scope.listPiliers = values[1].data;
+        $scope.listAxes = values[2].data;
+        $scope.listProjets = values[3].data;
+      }, err => {
+        console.log(err);
+      });
   })
