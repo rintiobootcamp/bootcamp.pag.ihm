5c7e5a3fe4ceee7d869855e3bf754eebe0297f79
Marius SODJINOU <desmart92@gmail.com>
2017-12-15 06:06:11 +0100

    Add limit images project

--- src/constantes/frontend.js
+++ src/constantes/frontend.js
@@ -1,4 +1,5 @@
 angular.module('pag-site')
     .constant("CONST",{
-        defaultImageEntity: 'https://lorempixel.com/580/250/nature/1'
+        defaultImageEntity: 'https://lorempixel.com/580/250/nature/1',
+        limitImgProjet : 11
     })
