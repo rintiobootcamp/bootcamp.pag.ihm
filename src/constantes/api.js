ca5ed2606b0c2399e64ef52581e7d41a6b989efd
Marius SODJINOU <desmart92@gmail.com>
2017-12-19 20:09:31 +0100

    Added new API url

--- src/constantes/api.js
+++ src/constantes/api.js
@@ -1,13 +1,13 @@
 angular.module('pag-site')
   .constant("API",{
     url: 'data/',
-    cat_fonct_url: 'http://165.227.69.188:8082/categorie/',
-    proj_fonct_url: 'http://165.227.69.188:8081/projet/projets',
-    media_fonct_url: 'http://165.227.69.188:8086/media/medias',
-    comment_fonct_url: 'http://165.227.69.188:8083/commentaire/commentaires',
-    note_fonct_url: 'http://165.227.69.188:8084/note/notes',
-    like_fonct_url: 'http://165.227.69.188:8085/like/likes',
-    sondage_fonct_url: 'http://165.227.69.188:8087/sondage/sondages',
+    cat_fonct_url: 'http://104.131.60.151/categorie/',
+    proj_fonct_url: 'http://104.131.60.151/projet/projets',
+    media_fonct_url: 'http://104.131.60.151/media/medias',
+    comment_fonct_url: 'http://104.131.60.151/commentaire/commentaires',
+    note_fonct_url: 'http://104.131.60.151/note/notes',
+    like_fonct_url: 'http://104.131.60.151/like/likes',
+    sondage_fonct_url: 'http://104.131.60.151/sondage/sondages',
     assets:''
   }
-  );
+);
