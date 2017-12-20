9452b6384519b88c0f4000c7af900f854b551aa0
Marius SODJINOU <desmart92@gmail.com>
2017-12-21 00:12:52 +0100

    Added Debat

--- src/controllers/site/ForumCtrl.js
+++ src/controllers/site/ForumCtrl.js
@@ -1,12 +1,208 @@
 angular.module('pag-site')
-  .controller("SiteForumCtrl", function ($scope,$q, ModelSecteur, ModelPilier, ModelAxe, ModelProjet) {
-    $q.all([ModelSecteur.list(),ModelPilier.list(),ModelAxe.list(), ModelProjet.list()])
+  .controller("SiteForumCtrl", function ($scope,$q, ModelSecteur, ModelPilier, ModelAxe, ModelProjet, ModelDebat) {
+    $q.all([ModelSecteur.list(),ModelPilier.list(),ModelAxe.list(), ModelProjet.list(),ModelDebat.list()])
       .then(values => {
         $scope.listSecteurs = values[0].data;
         $scope.listPiliers = values[1].data;
         $scope.listAxes = values[2].data;
         $scope.listProjets = values[3].data;
+
+        $scope.listDebats = values[4].data;
+        angular.forEach($scope.listDebats, function (debat, i) {
+          var secteur_debat = '';
+          switch (debat.entityType) {
+            case 'PILIER':
+              secteur_debat = _.filter($scope.listPiliers,{'id':debat.entityId})[0];
+              break;
+            case 'AXE':
+              secteur_debat = _.filter($scope.listAxes,{'id':debat.entityId})[0];
+            break;
+            case 'SECTEUR':
+              secteur_debat = _.filter($scope.listSecteurs,{'id':debat.entityId})[0];
+            break;
+            case 'PROJET':
+              secteur_debat = _.filter($scope.listProjets,{'id':parseInt(debat.entityId)})[0];
+            break;
+          
+            default:
+              break;
+          }
+          if(secteur_debat != undefined )
+            debat.secteur = secteur_debat.nom;
+          else debat.secteur = 'Non défini';
+        });
       }, err => {
         console.log(err);
       });
   })
+
+  .controller("SiteDebatCtrl", function (ModelMedia, ModelComment, $scope,$stateParams, CONST, API, Upload,$q, ModelLike, cookieModel, toaster, ModelDebat) {
+    var params_get_entity = {
+      entityId:$stateParams.id
+    }
+    var params_get_comments = {
+      entityId:$stateParams.id,
+      entityType: 'DEBAT'
+    }
+    
+    var getDebat = function (obj){
+      ModelDebat.get(obj)
+        .then(function (data){
+          $scope.debat = data.data;
+          var params_get_rubrique = {
+            entityId: data.data.entityId,
+            entityType: data.data.entityType
+          }
+          getListDebatRbrique(params_get_rubrique);
+        }, function (error){
+          console.log(error);
+        });
+    }
+    getDebat(params_get_entity);
+
+    var getListComments = function (params) {
+      ModelComment.list(params)
+        .then( function(data) {
+            $scope.listComments = data.data;
+            getMediaComment();
+        }, function (error) {
+            console.log(error);
+        });
+    }
+    getListComments(params_get_comments);
+
+    var getMediaComment = function (){
+      ModelMedia.getAll()
+        .then(function(data){
+            var data_medias = [];
+            angular.forEach($scope.listComments, function (comment, i){
+                angular.forEach(data.data, function (media, i){
+                    if(media.entityType == 'COMMENTAIRE') {
+                        if(media.entityId == comment.id){
+                            data_medias.push(media);
+                        }
+                    }
+                });
+                comment.medias = data_medias;
+                data_medias = [];
+            });
+        }, function (error){
+            console.log(error);
+        });
+    }
+
+    var getListDebatRbrique = function (params){
+      ModelDebat.getByEntity(params)
+        .then(function(data){
+          $scope.listOtherDebats = data.data;
+        }, function (error){
+          console.log(error);
+        });
+    }
+
+  $scope.comment = {
+    show:false,
+    id:0,
+    pseudo:'',
+    userMail:'',
+    contenu:'',
+    files:[]
+  }
+  if(cookieModel.getUser().pseudo != '') {
+      $scope.comment.pseudo = cookieModel.getUser().pseudo;
+  }
+
+  $scope.showComment = function (){
+      $scope.comment.show = true;
+  }
+
+  $scope.postComment = function (){
+      var params = {};
+          params.entityType = "PILIER";
+          params.entityId  = parseInt($stateParams.id);
+          if($scope.comment.pseudo !='')
+              params.pseudo = $scope.comment.pseudo;
+          if($scope.comment.userMail !='')
+              params.userMail = $scope.comment.userMail;
+          params.contenu = $scope.comment.contenu;
+          //console.log(params);
+
+           // Save Pseudo if user choose once
+          if($scope.comment.pseudo !=''){
+              cookieModel.setUser('pseudo',$scope.comment.pseudo);
+          }
+          // Save userMail if user choose once
+          if($scope.comment.userMail !=''){
+              cookieModel.setUser('email',$scope.comment.userMail);
+          }
+          // Check double action and Save to cookie
+          var checkCookie = cookieModel.getPilier();
+          if(checkCookie.comment.indexOf(params.entityId) === -1){
+              ModelComment.post(params)
+              .then(function(data){
+                  var setCookie = cookieModel.setPilier('comment',params.entityId);
+                  if(setCookie.STATUS === 300) {
+                      toogleToaster('error','Alerte',setCookie.STATUS.message);
+                  }
+                  if($scope.comment.files.length > 0) {
+                      uploadFiles($scope.comment.files, data.data.id);
+                  }else {
+                      getListComments(params_get_comments);
+                  }
+                  $scope.comment = {};
+              }, function (error){
+                  console.log(error);
+              });
+          }else {
+              toogleToaster('error','Alerte',"Vous avez déjà commenté ");
+          }
+  }
+  // upload on file select or drop
+  var uploadFile = function (file, idCommentaire) {
+      Upload.upload({
+          url: API.media_fonct_url + '/COMMENTAIRE/' + idCommentaire,
+          data: {file: file}
+      }).then(function (resp) {
+      }, function (resp) {
+
+      }, function (evt) {
+
+      });
+  };
+  // for multiple files:
+  var uploadFiles = function (files, idCommentaire) {
+    if (files && files.length) {
+      for (var i = 0; i < files.length; i++) {
+          uploadFile(files[i], idCommentaire);
+      }
+    }
+    getListComments(params_get_comments);
+  }
+
+  $scope.addCommentTinymceOptions =  {
+      onChange: function(e) {
+      },
+      height: 200,
+      menubar: false,
+      readonly:false,
+      plugins: [
+          'advlist autolink lists link image charmap print preview anchor textcolor',
+          'searchreplace visualblocks code',
+          'insertdatetime media table contextmenu code'
+        ],
+        toolbar: 'formatselect | bold italic underline strikethrough forecolor backcolor | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | removeformat',
+      skin: 'lightgray',
+      themes : 'modern',
+      language: 'fr_FR'
+  };
+
+  var toogleToaster = function (type,title, body){
+      toaster.pop({
+           type: type,
+           title: title,
+           body: body,
+           showCloseButton: true
+      })
+  }
+
+})
