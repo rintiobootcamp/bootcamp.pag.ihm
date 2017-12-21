bc0d9ef258e888689178cca5daf530c2eec6536e
Boris KOUMONDJI <brexis2009@yahoo.fr>
2017-12-21 11:37:41 +0100

    Complete chart with data

--- admin/src/directives/commentChartDirective.js
+++ admin/src/directives/commentChartDirective.js
@@ -2,16 +2,21 @@ angular.module('pag-site')
     .directive("commentChart", function() {
         return {
             restrict: 'E',
-            replace: true,
+            scope: {
+                data: "="
+            },
             templateUrl: '/admin/views/directives/bar-chart.html',
             link: function(scope, element) {
-                scope.labels = ['2006', '2007', '2008', '2009', '2010', '2011'];
-                scope.series = ['Series A', 'Series B'];
+                scope.labels = ['Nombre de commentaires', 'Nombre total de commentaires'];
+                scope.series3 = ['Serie A'];
+                scope.chartData = [];
 
-                scope.data = [
-                    [65, 59, 80, 81, 56, 55],
-                    [28, 48, 40, 19, 86, 27]
-                ];
+                scope.$watch('data', function() {
+                    console.log(scope.data);
+                    if(scope.data) {
+                        scope.chartData = [scope.data.nbreComment, scope.data.nbreTotalComment];
+                    }
+                });
             }
         }
     });
--- admin/src/directives/debatParPostChartDirective.js
+++ admin/src/directives/debatParPostChartDirective.js
@@ -2,15 +2,19 @@ angular.module('pag-site')
     .directive("debatParPostChart", function() {
         return {
             restrict: 'E',
-            replace: true,
+            scope: {
+                data: "="
+            },
             templateUrl: '/admin/views/directives/bar-chart.html',
             link: function(scope, element) {
-                scope.labels = ['2006', '2007', '2008', '2009', '2010', '2011'];
-                scope.series = ['Series A'];
+                scope.labels = ['Nombre Débats', 'Nombre total Débats'];
+                scope.chartData = [];
 
-                scope.data = [
-                    [65, 59, 80, 81, 56, 55]
-                ];
+                scope.$watch('data', function() {
+                    if(scope.data) {
+                        scope.chartData = [scope.data.nbreDebat, scope.data.nbreTotalDebat];
+                    }
+                });
             }
         }
     });
