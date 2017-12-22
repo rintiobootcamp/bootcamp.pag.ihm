d356a7eb782a5685df2a6bfe564c571ccbf695e2
Boris KOUMONDJI <brexis2009@yahoo.fr>
2017-12-22 09:54:22 +0100

    Fix secteur creation

--- admin/src/controllers/admin/SecteursCtrl.js
+++ admin/src/controllers/admin/SecteursCtrl.js
@@ -1,56 +1,56 @@
 angular.module('pag-site')
-.controller("AdminSecteursCtrl", function (ModelSecteur, $scope) {
-  $scope.deleteSect = function(secteur_id) {
-    console.log('Secteur delete function ', secteur_id);
-    ModelSecteur.delete(secteur_id);
-  }
-  var getListSecteurs = function () {
-    ModelSecteur.list()
-    .then( function(data) {
-      $scope.listSecteurs = data.data;
-      console.log('$scope.listSecteurs ', $scope.listSecteurs);
-    }, function (error) {
-      console.log(error);
+    .controller("AdminSecteursCtrl", function(ModelSecteur, $scope) {
+        $scope.deleteSect = function(secteur_id) {
+            console.log('Secteur delete function ', secteur_id);
+            ModelSecteur.delete(secteur_id);
+        }
+        var getListSecteurs = function() {
+            ModelSecteur.list()
+                .then(function(data) {
+                    $scope.listSecteurs = data.data;
+                    console.log('$scope.listSecteurs ', $scope.listSecteurs);
+                }, function(error) {
+                    console.log(error);
+                });
+        }
+        getListSecteurs();
+    })
+    .controller("AdminNewSecteurCtrl", function(ModelSecteur, ModelPilier, $scope, $stateParams) {
+        console.log('Admin Nouveau secteur controller');
+        let secteur_id = $stateParams.id;
+        $scope.save = secteur_id ? 'Mettre à jour' : 'Terminer';
+        var getListPiliers = function() {
+            ModelPilier.list()
+                .then(function(data) {
+                    $scope.listPiliers = data.data;
+                }, function(error) {
+                    console.log(error);
+                });
+        }
+        getListPiliers();
+
+        $scope.pilier = {
+            name: ''
+        };
+    })
+    .controller("AdminEditSecteurCtrl", function(ModelSecteur, $rootScope, $scope, $stateParams, $state) {
+        console.log('Admin edit secteur controller');
+
+        let secteur_id = $stateParams.id;
+        $scope.save = secteur_id ? 'Mettre à jour' : 'Terminer';
+        $scope.sect_title = secteur_id ? 'Modifier le secteur' : 'Nouveau secteur';
+
+        $rootScope.loading = true;
+        $scope.secteur = {};
+
+        ModelSecteur.get(secteur_id).then(function(s) {
+            $scope.secteur = s.data;
+            $rootScope.loading = false;
+        });
+
+        $scope.saveSecteur = function() {
+            ModelSecteur.save($scope.secteur).then(function() {
+                $state.go('admin.secteurs');
+            });
+        }
     });
-  }
-  getListSecteurs();
-})
-.controller("AdminNewSecteurCtrl", function (ModelSecteur, ModelPilier, $scope, $stateParams) {
-  console.log('Admin Nouveau secteur controller');
-  let secteur_id = $stateParams.id;
-  $scope.save = secteur_id ? 'Mettre à jour' : 'Terminer';
-  var getListPiliers = function () {
-    ModelPilier.list()
-    .then( function(data) {
-      $scope.listPiliers = data.data;
-    }, function (error) {
-      console.log(error);
-    });
-  }
-  getListPiliers();
-
-  $scope.pilier = {name: ''};
-})
-.controller("AdminEditSecteurCtrl", function (ModelSecteur, $rootScope, $scope, $stateParams) {
-  console.log('Admin edit secteur controller');
-
-  let secteur_id = $stateParams.id;
-  $scope.save = secteur_id ? 'Mettre à jour' : 'Terminer';
-  $scope.sect_title = secteur_id ? 'Modifier le secteur' : 'Nouveau secteur';
-
-  $rootScope.loading = true;
-  $scope.GetSecteur = ModelSecteur.get(secteur_id).then(function (s) {
-    let data = s.data
-    $scope.nom = data.nom;
-    $scope.desc = data.description;
-    $rootScope.loading = false;
-  });
-
-  // $scope.Update = function () {
-  //     $rootScope.loading = true;
-  //     PDService.Update($scope.RecordToEdit, $scope.firstName, $scope.lastName, $scope.age, $scope.active).then(function (d) {
-  //         $scope.UpdateMessage = d;
-  //         $rootScope.loading = false;
-  //     });
-  // };
-});
--- admin/src/services/SecteurFactory.js
+++ admin/src/services/SecteurFactory.js
@@ -1,37 +1,30 @@
 angular.module('pag-site')
-  .factory('ModelSecteur', function ($http, API) {
-    return {
-      get: function (id){
-        return $http({
-          method: 'GET',
-          url: API.cat_fonct_url + 'secteurs/'+ id
-        });
-      },
-      list: function (){
-        return $http({
-          method: 'GET',
-          url: API.cat_fonct_url + 'secteurs'
-        });
-      },
-      create: function(secteur) {
-        return $http({
-          method: 'POST',
-          url: API.url + 'secteur',
-          data: angular.toJson(secteur)
-        })
-      },
-      update: function(secteur) {
-        return $http({
-          method: 'POST',
-          url: API.url + 'secteur/' + secteur.id,
-          data: angular.toJson(secteur)
-        });
-      },
-      delete(secteur_id) {
-        return $http({
-          method: 'DELETE',
-          url: API.cat_fonct_url + 'secteurs/' + secteur_id
-        })
-      }
-    }
-  });
+    .factory('ModelSecteur', function($http, API) {
+        return {
+            get: function(id) {
+                return $http({
+                    method: 'GET',
+                    url: API.cat_fonct_url + 'secteurs/' + id
+                });
+            },
+            list: function() {
+                return $http({
+                    method: 'GET',
+                    url: API.cat_fonct_url + 'secteurs'
+                });
+            },
+            save: function(secteur) {
+                return $http({
+                    method: secteur.id ? 'PUT' : 'POST',
+                    url: API.cat_fonct_url + 'secteurs',
+                    data: angular.toJson(secteur)
+                })
+            },
+            delete(secteur_id) {
+                return $http({
+                    method: 'DELETE',
+                    url: API.cat_fonct_url + 'secteurs/' + secteur_id
+                })
+            }
+        }
+    });
