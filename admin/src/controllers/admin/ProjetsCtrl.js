f4750fb68679c7900e0fdf5b6147deb2dcbaef86
Boris KOUMONDJI <brexis2009@yahoo.fr>
2017-12-22 10:49:54 +0100

    Update project creation

--- admin/src/controllers/admin/ProjetsCtrl.js
+++ admin/src/controllers/admin/ProjetsCtrl.js
@@ -17,43 +17,30 @@ angular.module('pag-site')
         getListProjets();
     })
     .controller("AdminNewProjetCtrl", function(ModelPilier, $scope, $stateParams) {
-        let projet_id = $stateParams.id;
-        $scope.save = projet_id ? 'Mettre à jour' : 'Terminer';
-        console.log('Admin Nouveau projet controller');
-        var getListPiliers = function() {
-            ModelPilier.list()
-                .then(function(data) {
-                    $scope.listPiliers = data.data;
-                }, function(error) {
-                    console.log(error);
-                });
-        }
-        getListPiliers();
+        $scope.saveProject = function() {
+            ModelProjet.save($scope.projet).then(function() {
+                $state.go('admin.projets');
+            });
+        };
 
         $scope.pilier = {
             name: ''
         };
     })
-    .controller("AdminEditProjetCtrl", function(ModelProjet, $rootScope, $scope, $stateParams) {
-        console.log('Admin edit projet controller');
-
-        let projet_id = $stateParams.id;
-        $scope.save = projet_id ? 'Mettre à jour' : 'Terminer';
-        $scope.proj_title = projet_id ? 'Modifier le projet' : 'Nouveau projet';
+    .controller("AdminEditProjetCtrl", function(ModelProjet, $rootScope, $scope, $stateParams, $state) {
+        let id = $stateParams.id;
+        $scope.save = id ? 'Mettre à jour' : 'Terminer';
+        $scope.proj_title = id ? 'Modifier le projet' : 'Nouveau projet';
         $rootScope.loading = true;
-        $scope.GetProjet = ModelProjet.get(projet_id).then(function(p) {
-            let data = p.data;
-            $scope.titre = data.nom;
-            $scope.desc = data.description;
-            $scope.dateD = data.dateDebutReel;
-            $scope.dateF = data.dateFinReel;
-            $scope.budget = data.budgetPrevisionnel;
-            $scope.cout = data.coutReel;
-            $scope.dateDp = data.dateDebutPrevisionnel;
-            $scope.dateFp = data.dateFinPrevisionnel;
-            $scope.reference = data.reference;
-            $scope.financementPr = data.financementPublic;
-            $scope.financementPu = data.financementPrive;
+
+        ModelProjet.get(id).then(function(p) {
+            $scope.projet = p.data;
             $rootScope.loading = false;
         });
+
+        $scope.saveProject = function() {
+            ModelProjet.save($scope.projet).then(function() {
+                $state.go('admin.projets');
+            });
+        };
     });
