b57f88cbeb377f51ee59f405bb986ada72911747
Marius SODJINOU <desmart92@gmail.com>
2018-01-16 17:25:34 +0100

    Review note

--- src/controllers/site/PilierCtrl.js
+++ src/controllers/site/PilierCtrl.js
@@ -311,15 +311,12 @@ angular.module('pag-site')
         ModelNote.get(params_get_comments)
         .then(function (data) {
             $scope.noteEntity = data.data;
-            $scope.noteEntity.total = $scope.noteEntity.noteFiveCounts
-             + $scope.noteEntity.noteFourCounts + $scope.noteEntity.noteOneCounts
-             + $scope.noteEntity.noteThreeCounts + $scope.noteEntity.noteTwoCounts;
-            $scope.noteEntity.notes = {};
-            $scope.noteEntity.notes[1] = $scope.noteEntity.noteOneCounts;
-            $scope.noteEntity.notes[2] = $scope.noteEntity.noteTwoCounts;
-            $scope.noteEntity.notes[3] = $scope.noteEntity.noteThreeCounts;
-            $scope.noteEntity.notes[4] = $scope.noteEntity.noteFourCounts;
-            $scope.noteEntity.notes[5] = $scope.noteEntity.noteFiveCounts;
+            $scope.noteEntity.notes = [];
+            $scope.noteEntity.notes.push({i: 1,value: $scope.noteEntity.noteOneCounts});
+            $scope.noteEntity.notes.push({i: 2,value: $scope.noteEntity.noteTwoCounts});
+            $scope.noteEntity.notes.push({i: 3,value: $scope.noteEntity.noteThreeCounts});
+            $scope.noteEntity.notes.push({i: 4,value: $scope.noteEntity.noteFourCounts});
+            $scope.noteEntity.notes.push({i: 5,value: $scope.noteEntity.noteFiveCounts});
 
             console.log($scope.noteEntity);
         });
